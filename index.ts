3a28d997ee73d2ea6116822f032956a7e70e989b
shrenik49 <sutarshrenik3@gmail.com>
2023-10-12 16:22:36 +0530

    note-app-server

---
+++ index.ts
@@ -0,0 +1,82 @@
+import express from "express";
+import cors from "cors";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+//get all the data from the database
+app.get("/api/notes", async (req, res) => {
+  const notes = await prisma.note.findMany();
+  res.json(notes);
+});
+
+//post the data to database
+app.post("/api/notes", async (req, res) => {
+  const { title, content } = req.body;
+
+  if (!title || !content) {
+    return res.status(400).send("title and content fields required");
+  }
+
+  try {
+    const note = await prisma.note.create({
+      data: { title, content },
+    });
+    res.json(note);
+  } catch (error) {
+    res.status(500).send("Oops, something went wrong");
+  }
+});
+
+// update the notes in the database
+app.put("/api/notes/:id", async (req, res) => {
+  const { title, content } = req.body;
+  const id = parseInt(req.params.id);
+
+  if (!title || !content) {
+    return res.status(400).send("title and content fields required");
+  }
+
+  if (!id || isNaN(id)) {
+    return res.status(400).send("ID must be a valid number");
+  }
+
+  try {
+    const updatedNote = await prisma.note.update({
+      where: { id },
+      data: { title, content },
+    });
+    res.json(updatedNote);
+  } catch (error) {
+    res.status(500).send("Oops, something went wrong");
+  }
+});
+
+// delete the note from the database
+app.delete("/api/notes/:id", async (req, res) => {
+  const id = parseInt(req.params.id);
+
+  if (!id || isNaN(id)) {
+    return res.status(400).send("ID field required");
+  }
+
+  try {
+    await prisma.note.delete({
+      where: { id },
+    });
+    res.status(204).send();
+  } catch (error) {
+    res.status(500).send("Oops, something went wrong");
+  }
+});
+// app.get("/api/notes", async (req, res) => {
+//   res.json({ message: "success!" });
+// });
+
+app.listen(5000, () => {
+  console.log("server running on localhost:5000");
+});
